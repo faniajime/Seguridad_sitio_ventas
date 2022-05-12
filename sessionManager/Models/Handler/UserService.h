20d95843f2c341a4a2154ee424539e2b6d03f44a
Fabiola Jiménez-González <45375479+faniajime@users.noreply.github.com>
2022-05-11 19:26:20 -0600

    Create UserService.h

---
+++ sessionManager/Models/Handler/UserService.h
@@ -0,0 +1,22 @@
+#ifndef userService_H
+#define userService_H
+
+#include <string>
+#include "Database.h"
+#include <stdio.h>
+#include<stdlib.h>
+#include <iostream>
+#include <mysql.h>
+
+using namespace std;
+
+class UserService
+{
+    public:
+    UserService ();
+    ~UserService();
+     MYSQL *con;
+    createUser(string nombre, string usuario, string email, int telefono, string contrasena, string direccion);
+
+};
+#endif
