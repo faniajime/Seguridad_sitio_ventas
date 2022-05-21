7d4bd35a7dadfa7a5f2af0b9c62f1e79bea87f2c
Karen <karen.zamoravillalobos@ucr.ac.cr>
2022-05-20 20:43:33 -0600

    Added addProductView

---
+++ Views/addProductView.h
@@ -0,0 +1,17 @@
+#ifndef ADDPRODUCTVIEW_H
+#define ADDPRODUCTVIEW__H
+
+using namespace std;
+
+class addProductView{
+
+	public:
+
+	addProductView();
+	~addProductView();
+	 void printPage();
+
+	private:
+
+};
+#endif
