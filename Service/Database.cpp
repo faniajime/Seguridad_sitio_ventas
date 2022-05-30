71b527faf5ab570cfba9eb71d849d473c7aec9c3
root <root@vm-131-195.unac.ucr.ac.cr>
2022-05-29 20:20:22 -0600

    changing database to run on cent0s

--- Service/Database.cpp
+++ Service/Database.cpp
@@ -14,8 +14,8 @@ bool Database::connectToDatabase(){
   bool connected = true;
   const char* MY_HOSTNAME = "localhost";
   const char* MY_DATABASE = "hackeques";
-  const char* MY_USERNAME = "padmin";
-  const char* MY_PASSWORD = "super-strong-password";
+  const char* MY_USERNAME = "root";
+  const char* MY_PASSWORD = "";
   const char* MY_SOCKET = NULL;
   
   conn = mysql_init(NULL);
