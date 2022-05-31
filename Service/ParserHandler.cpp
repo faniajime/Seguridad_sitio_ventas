e6aecfd37ff5809d3b848abdf7c543a3b2750f28
Steve Moon <56247331+sungjaemoonUCR@users.noreply.github.com>
2022-05-31 16:56:47 -0600

    Update ParserHandler.cpp

--- Service/ParserHandler.cpp
+++ Service/ParserHandler.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 ParserHandler::ParserHandler(){
-   // Initialize private variables
+
    ArgCnt = 0;
    for (int pos = 0; pos < MAX_ARGS; pos++)
       Name[pos] = Value[pos] = NULL;
@@ -15,25 +15,25 @@ ParserHandler::~ParserHandler(){}
 
 
 void ParserHandler::parseQuery (int queryLength, char* queryString){
-// Separate query_string into arguments 
+
    int start_name, end_name, start_value, end_value = -1;
    while (end_value < queryLength)
    {
-      // Find argument name
+
       start_name = end_name = end_value + 1;
       while ((end_name<queryLength) && (queryString[end_name] != '='))
          end_name++;
    
-      // Copy and decode name string
+
       Name[ArgCnt] = copy_string(queryString, start_name, end_name);
       decode_string(Name[ArgCnt]);
 
-      // Find argument value
+
       start_value = end_value = end_name + 1;
       while ((end_value<queryLength) && (queryString[end_value] != '&'))
          end_value++;
 
-      // Copy and decode value string
+
       Value[ArgCnt] = copy_string(queryString, start_value, end_value);
       decode_string(Value[ArgCnt]);
       ArgCnt++;
@@ -41,12 +41,11 @@ void ParserHandler::parseQuery (int queryLength, char* queryString){
 }
 char* ParserHandler::copy_string(char *str, int start, int end)
 {
-   // Create string
    char *copy = (char *)malloc(end-start+1);
    if (copy == NULL) 
       return NULL;
 
-   // Copy substring
+
    int pos;
    for (pos = start; pos < end; pos++)
       copy[pos-start] = str[pos];
@@ -54,9 +53,7 @@ char* ParserHandler::copy_string(char *str, int start, int end)
    return copy;
 }
 
-
-// Function for decoding CGI strings.
-
+// para manejar digitos hex
 void ParserHandler::decode_string(char *str)
 {
    const char *digits = "0123456789ABCDEF";
@@ -78,43 +75,41 @@ void ParserHandler::decode_string(char *str)
                        + strchr(digits, ch2)-strchr(digits,'0'); 
       }
 
-      // Handle remaining characters
-      else
-         str[outpos++] = str[pos];
+
+      else {
+               str[outpos++] = str[pos];
+      }
+
    }
 
-   // Mark end of string
+
    str[outpos] = '\0';
 }
 
 
-// Argument lookup by name.
+// busca el url por nombre
 char *ParserHandler::GetArg(const char name[])
 {
-   // Lookup argument by name
+
    for (int arg=0; arg<ArgCnt; arg++)
       if (strcmp(name, Name[arg]) == 0)
          return Value[arg];
 
-   // Return NULL if not found
+
    return NULL;
 }
 
-//--------------------------------------------------------------
-// Argument name lookup by number.
-//--------------------------------------------------------------
+//se trae la llave del url
 char *ParserHandler::GetName(int index)
 {
-   // Lookup argument by location
+
    if ((index >= 0) && (index < ArgCnt))
       return Name[index];
    else 
       return NULL;
 }
 
-//--------------------------------------------------------------
-// Argument value lookup by number.
-//--------------------------------------------------------------
+// se trae el valor del url del nombre
 char *ParserHandler::GetValue(int index)
 {
    // Lookup argument by location
@@ -124,9 +119,7 @@ char *ParserHandler::GetValue(int index)
       return NULL;
 }
 
-//--------------------------------------------------------------
-// Return number of CGI arguments.
-//--------------------------------------------------------------
+
 int ParserHandler::GetCnt()
 {
    return ArgCnt;
